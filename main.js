92391359fc76e21bc1bb1610d69d3cfba08266cb
Richard Jarram <rjarram@me.com>
2019-09-10 20:10:15 -0400

    built adjacent cell algorithm

--- main.js
+++ main.js
@@ -3,24 +3,39 @@ const toggleCell = (cell) => {
   cell.classList.add('opened');
 }
 
+const getAdjacentCells = (cell) => {
+  let c1 = document.getElementById(`${cell.parentElement.rowIndex - 1}-${cell.cellIndex + 1}`);
+  let c2 = document.getElementById(`${cell.parentElement.rowIndex}-${cell.cellIndex + 1}`);
+  let c3 = document.getElementById(`${cell.parentElement.rowIndex + 1}-${cell.cellIndex + 1}`);
+  let c4 = document.getElementById(`${cell.parentElement.rowIndex - 1}-${cell.cellIndex}`);
+  let c5 = document.getElementById(`${cell.parentElement.rowIndex + 1}-${cell.cellIndex}`);
+  let c6 = document.getElementById(`${cell.parentElement.rowIndex - 1}-${cell.cellIndex - 1}`);
+  let c7 = document.getElementById(`${cell.parentElement.rowIndex}-${cell.cellIndex - 1}`);
+  let c8 = document.getElementById(`${cell.parentElement.rowIndex + 1}-${cell.cellIndex - 1}`);
+  return [c1,c2,c3,c4,c5,c6,c7,c8].filter(c => c !== null)
+}
+
 const checkAdjacentCells = (cell) => {
-  // const cellIndex = cell.cellIndex;
-  // const cellRow = cell.parentElement.rowIndex;
-  // console.log(cellIndex, cellRow);
-
   // Select the x8 adjacent cells of 'cell'.
-  let adjacentCells = document.querySelectorAll('');
+  let clickedX = cell.parentElement.rowIndex;
+  let clickedY = cell.cellIndex;
+  let adjacentCells = [];
 
-  condition = true
+  console.log(getAdjacentCells(cell));
+
+  // console.log(cell.getAttribute('id'));
+  // console.log(clickedX, clickedY);
+
+  // condition = true
   // while some cell in the entire table is such that:
     // it has an adjacent cell such that:
       // It is both NOT-opened AND has no adjacent mine && has no adjacent-numbered-cell
-  if (condition) {
-    // execute the checker function again and update the array.
-    // checkAdjacentCells(newCell);
-  } else {
-    // end the function
-  }
+  // if (condition) {
+  //   // execute the checker function again and update the array.
+  //   // checkAdjacentCells(newCell);
+  // } else {
+  //   // end the function
+  // }
 }
 
 const click = (cell) => {
@@ -39,3 +54,8 @@ unopened.forEach(cell => {
     click(cell);
   })
 });
+
+// Code for cell indices
+// const cellIndex = cell.cellIndex;
+// const cellRow = cell.parentElement.rowIndex;
+// console.log(cellIndex, cellRow);
